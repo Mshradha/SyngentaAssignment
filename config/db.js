ba5956225a6be058a9cafb15b83440cee5c46881
Shradha Mishra <99673738+Mshradha@users.noreply.github.com>
2022-10-14 22:20:33 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const connection =  mongoose.connect("mongodb+srv://Mshradha17:Wisj5YopJchd1h5r@cluster0.nczxmpj.mongodb.net/organisation?retryWrites=true&w=majority");
-
-module.exports = connection;
